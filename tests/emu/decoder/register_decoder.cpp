ccc177716a72d77ba976ca7797b898d335cc3bb8
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2016-05-14 13:46:32 -0300

    Adiciona teste que nao passa pra testar Travis

--- tests/emu/decoder/register_decoder.cpp
+++ tests/emu/decoder/register_decoder.cpp
@@ -10,4 +10,5 @@ TEST(register_decoder, getOpCode) {
     instruction32_t instruction = 0xac000000;
     RegisterInstructionDecoder decoder;
     RegisterInstruction *emu_instruction = (RegisterInstruction*) decoder.decode(instruction);
+    ASSERT_EQ(2, 3);
 }
