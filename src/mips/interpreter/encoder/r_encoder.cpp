56c451f1a35cd2983d76231bab1b3823801ab974
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2016-05-29 22:24:50 -0300

    Teste com cobertura de testes

--- src/mips/interpreter/encoder/r_encoder.cpp
+++ src/mips/interpreter/encoder/r_encoder.cpp
@@ -3,5 +3,22 @@
 using namespace MIPS;
 
 instruction32_t REncoder::encode(std::vector<char*>& params) {
-	return 0;
+	bit8_t opcode, r1, r2, r3, funct;
+	struct InstructionInfo info;
+	instruction32_t instruction;
+	info = getInstructionInfo(params.at(0));
+	opcode = info.opcode;
+	funct = info.funct;
+	r1 = getRegisterNumber(params.at(1));
+	r2 = getRegisterNumber(params.at(2));
+	r3 = getRegisterNumber(params.at(3));
+	// Forma a instrução 32 bits
+	instruction += opcode;
+	instruction <<= 6;
+	instruction += r1;
+	instruction <= 5;
+	instruction += r2;
+	instruction <= 5;
+	instruction += r3;
+	instruction <= 5;
 }
