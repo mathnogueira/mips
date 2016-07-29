e93ae043319a8c43f3f6dad11a665426ecf53c14
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2016-07-29 20:57:46 -0300

    Teste de todos os casos do formato IV

--- tests/mips/interpreter/encoder/format_IV_encoder.cpp
+++ tests/mips/interpreter/encoder/format_IV_encoder.cpp
@@ -3,7 +3,17 @@
 
 using namespace MIPS;
 
-TEST(FormatIVEncoder, encodeJT_zero) {
+TEST(FormatIVEncoder, encodeJF_neg) {
+	std::vector<char*> params;
+	params.push_back("jf.neg");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 1149);
+}
+
+TEST(FormatIVEncoder, encodeJF_zero) {
 	std::vector<char*> params;
 	params.push_back("jf.zero");
 	params.push_back("125");
@@ -13,7 +23,37 @@ TEST(FormatIVEncoder, encodeJT_zero) {
 	ASSERT_EQ(instruction, 1405);
 }
 
-TEST(FormatIVEncoder, encodeJT_overflow) {
+TEST(FormatIVEncoder, encodeJF_carry) {
+	std::vector<char*> params;
+	params.push_back("jf.carry");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 1661);
+}
+
+TEST(FormatIVEncoder, encodeJF_negzero) {
+	std::vector<char*> params;
+	params.push_back("jf.negzero");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 1917);
+}
+
+TEST(FormatIVEncoder, encodeJF_true) {
+	std::vector<char*> params;
+	params.push_back("jf.true");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 125);
+}
+
+TEST(FormatIVEncoder, encodeJF_overflow) {
 	std::vector<char*> params;
 	params.push_back("jf.overflow");
 	params.push_back("128");
@@ -21,4 +61,64 @@ TEST(FormatIVEncoder, encodeJT_overflow) {
 	encoder.parse(params);
 	instruction_t instruction = encoder.encode();
 	ASSERT_EQ(instruction, 896);
+}
+
+TEST(FormatIVEncoder, encodeJT_neg) {
+	std::vector<char*> params;
+	params.push_back("jt.neg");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 5245);
+}
+
+TEST(FormatIVEncoder, encodeJT_zero) {
+	std::vector<char*> params;
+	params.push_back("jt.zero");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 5501);
+}
+
+TEST(FormatIVEncoder, encodeJT_carry) {
+	std::vector<char*> params;
+	params.push_back("jt.carry");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 5757);
+}
+
+TEST(FormatIVEncoder, encodeJT_negzero) {
+	std::vector<char*> params;
+	params.push_back("jt.negzero");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 6013);
+}
+
+TEST(FormatIVEncoder, encodeJT_true) {
+	std::vector<char*> params;
+	params.push_back("jt.true");
+	params.push_back("125");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 4221);
+}
+
+TEST(FormatIVEncoder, encodeJT_overflow) {
+	std::vector<char*> params;
+	params.push_back("jt.overflow");
+	params.push_back("128");
+	FormatIVEncoder encoder;
+	encoder.parse(params);
+	instruction_t instruction = encoder.encode();
+	ASSERT_EQ(instruction, 4992);
 }
