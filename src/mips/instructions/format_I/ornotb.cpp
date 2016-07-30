9a6cc34de2053c7067db07a2971ed2695b1e4fd0
Lucas Pereira <lpfcomputacao@gmail.com>
2016-07-30 01:01:08 -0300

    Ornotb.cpp/hpp/test

---
+++ src/mips/instructions/format_I/ornotb.cpp
@@ -0,0 +1,12 @@
+#include <mips/instructions/format_I/ornotb.hpp>
+
+using namespace MIPS;
+
+bit16_t OrnotbInstruction::execute() {
+	bit16_t rs = this->rs->get();
+	bit16_t rt = this->rt->get();
+	bit16_t result = rs | !(rt);
+	return result;
+}
+
+
