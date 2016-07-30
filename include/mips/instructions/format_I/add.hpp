bf2a2bf71cfce01eb512de152026262320f08a89
Felipe Dias <fsdias.v@gmail.com>
2016-07-29 22:06:08 -0300

    Implementação das instruções ADD e ADDINC

--- include/mips/instructions/format_I/add.hpp
+++ include/mips/instructions/format_I/add.hpp
@@ -12,15 +12,26 @@ namespace MIPS {
 /**
  * Classe que faz a operação de ADD no processador.
  *
- * \author Debora Rossini
+ * \author Felipe Dias
  */
 class AddInstruction : public InstructionI {
 
 public:
+
+	/**
+	 * Constroi uma nova instrução.
+	 */
+	 AddInstruction(bit8_t opcode,
+                         Register *rs,
+                         Register *rt,
+                         bit8_t shamt,
+                         bit8_t funct)
+	: InstructionI(opcode, rs, rt, shamt, funct) {}
+
 	/**
 	 * Função que executa a operação de soma.
 	 *
-	 * \return resultado da soma
+	 * \return resultado da operação
 	 */
 	bit16_t execute();
 
--- src/mips/instructions/format_I/add.cpp
+++ src/mips/instructions/format_I/add.cpp
@@ -1,9 +1,12 @@
 #include <mips/instructions/format_I/add.hpp>
+#include <mips/circuits/full_adder.hpp>
 
 using namespace MIPS;
 
 bit16_t AddInstruction::execute() {
-	// TEM QUE FAZER EM BINARIO
-	// rd = rs + rt;
-	return 0;
+    FullAdder sum;
+    
+    bit16_t result = sum.add(this->rs->get(), this->rt->get(), 0);
+    
+    return result;
 }
