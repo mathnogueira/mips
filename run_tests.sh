9e1e886caf8e61320840873b0cc66d878e5192a9
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2016-08-02 13:30:04 -0300

    Adiciona case de testes

---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+./build/mips tests/programs/divide.asm
+./build/mips tests/programs/jump.asm
+./build/mips tests/programs/logica.asm
+./build/mips tests/programs/multiplica.asm
+./build/mips tests/programs/shift.asm
