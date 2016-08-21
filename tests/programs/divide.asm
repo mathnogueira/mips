a02987b745c7e068623350f4c848aefe1096a82f
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2016-08-21 18:02:06 -0300

    Conserta script do travis

--- tests/programs/divide.asm
+++ tests/programs/divide.asm
@@ -2,18 +2,20 @@
 ; Este programa divide um numero inteiro de ate 16 bits por outro
 ; tambem de 16 bits e armazena o resultado em Mem[1000]
 
-	lcl r2, 1500		; Divisor
-	lch r2, 1500
+	lcl r2, 50		; Divisor
+	lch r2, 50
 	lcl r3, 5		; Dividendo
 	lch r3, 5
 	zeros r4
 
-LOOP:   sub r2,r2,r3
-        jt.neg SAIDA
+LOOP:
+	sub r2,r2,r3
+    jt.neg SAIDA
 	inca r4,r4
 	j LOOP
 
-SAIDA: 	loadlit r3,1000
+SAIDA:
+	loadlit r3,1000
 	store r3,r4
 
 HALT: j HALT
